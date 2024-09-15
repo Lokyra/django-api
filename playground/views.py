7c794954493b08a43006af03f93bb8d51e807ec9
ayoub <ayoubousbia02@gmail.com>
2024-09-15 21:20:17 +0200

    update view

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from store.models import Order, OrderItem, Product, Customer
 
 # Create your views here.
 
 
 def say_hello(request):
-    return render(request, "hello.html")
+    pass
